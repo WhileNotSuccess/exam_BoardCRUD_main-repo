6f5261c7aca10eed2be73051f02432ec28bf703f
hyenseochoi <whahoo1@naver.com>
2024-12-10 14:44:30 +0900

    ✨feat: nested-comment remove 추가, 사용자 Req 객체 이름 설정, 트랜잭션 수정

--- nest/src/nested-comment/nested-comment.service.ts
+++ nest/src/nested-comment/nested-comment.service.ts
@@ -6,41 +6,79 @@ import { DataSource } from 'typeorm';
 
 @Injectable()
 export class NestedCommentService {
-  constructor(private readonly dataSource : DataSource){}
-  
+  constructor(private readonly dataSource: DataSource) {}
 
-  
-  async create(createNestedCommentDto: CreateNestedCommentDto) {
-    const queryRunner = this.dataSource.createQueryRunner()
-    await queryRunner.connect()
-    await queryRunner.startTransaction()
+
+  async create(createNestedCommentDto: CreateNestedCommentDto,req:string) {
+    
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
     
     try {
-      const a = queryRunner.manager.create(NestedComment,createNestedCommentDto)
-      a.author = "진모"
-      await queryRunner.manager.save(a)
-      await queryRunner.commitTransaction()
-
-    }catch(e){
-      await queryRunner.rollbackTransaction()
-    
-    }finally{
-      await queryRunner.release()
-    
+      const comment = queryRunner.manager.create(NestedComment, createNestedCommentDto);
+      comment.author = req
+      await queryRunner.manager.save(comment);
+      await queryRunner.commitTransaction();
+      return { message: '대댓글 추가를 성공했습니다' };
+    } catch (e) {
+      console.error('대댓글 추가 에러 :', e);
+      await queryRunner.rollbackTransaction();
+      throw e;
+    } finally {
+      await queryRunner.release();
     }
-    return {message:"성공입니다"}
   }
 
   async findAll(commentId: number) {
-    return await this.dataSource.manager.findBy(NestedComment,{commentId:commentId});
+    return await this.dataSource.manager.findBy(NestedComment, { commentId: commentId });
   }
 
   async findOne(id: number) {
-    return await this.dataSource.manager.findOneBy(NestedComment,{id:id})
+    return await this.dataSource.manager.findOneBy(NestedComment, { id: id });
   }
-  
-  update(id: number, updateNestedCommentDto: UpdateNestedCommentDto) {
-    return `This action updates a #${id} nestedComment`;
+
+  async update(id: number, updateNestedCommentDto: UpdateNestedCommentDto) {
+
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      
+
+      await queryRunner.manager.update(NestedComment, id, updateNestedCommentDto);
+      await queryRunner.commitTransaction();
+      return { message: '수정을 성공했습니다.' };
+    } catch (e) {
+      console.error('수정 실패 :', e);
+      await queryRunner.rollbackTransaction();
+      throw e;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async remove(id: number) {
+
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.manager.delete(NestedComment, id);
+      await queryRunner.commitTransaction();
+      return { message: '삭제를 성공했습니다.' };
+    } catch (e) {
+      console.error('삭제 실패 :', e);
+      await queryRunner.rollbackTransaction();
+      throw e;
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async LHsremove(id: number[]) { // 배열을 받고 조건은 id in 배열
