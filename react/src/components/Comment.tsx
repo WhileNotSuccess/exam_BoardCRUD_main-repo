3229677ad3ab21005adc1df3e4b30290c5ed950f
kasinito22 <kasinito@g.yju.ac.kr>
2024-12-14 12:56:35 +0900

    Update Comment.tsx

--- react/src/components/Comment.tsx
+++ react/src/components/Comment.tsx
@@ -102,7 +102,7 @@ const Comment: React.FC<CommentProps> = ({
             ) : (
               <>
                 {/*click시 commentId에 댓글 아이디 저장, nestedAppear false=>true*/}
-                {!user?(
+                {user?(
                 <button
                   onClick={() => {
                     setCommentId(data.id);
